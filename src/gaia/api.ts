c527a503485dff38d45ef17bb60f092ab14c987e
yunjh1994 <yunjh1994@gmail.com>
2019-07-19 17:10:35 +0900

    Make user override gaia api's core config

--- src/gaia/api.ts
+++ src/gaia/api.ts
@@ -1,4 +1,4 @@
-import { Api, ApiConfig } from "../core/api";
+import { Api, ApiConfig, CoreConfig } from "../core/api";
 import { GaiaRest } from "./rest";
 import { defaultTxEncoder } from "../common/stdTx";
 import { stdTxBuilder } from "../common/stdTxBuilder";
@@ -8,21 +8,27 @@ import { BIP44 } from "../core/bip44";
 import { defaultBech32Config } from "../core/bech32Config";
 
 export class GaiaApi extends Api<GaiaRest> {
-  constructor(config: ApiConfig) {
+  constructor(
+    config: ApiConfig,
+    coreConfig: Partial<CoreConfig<GaiaRest>> = {}
+  ) {
     super(config, {
-      txEncoder: defaultTxEncoder,
-      txBuilder: stdTxBuilder,
-      restFactory: (context: Context) => {
-        return new GaiaRest(context);
+      ...{
+        txEncoder: defaultTxEncoder,
+        txBuilder: stdTxBuilder,
+        restFactory: (context: Context) => {
+          return new GaiaRest(context);
+        },
+        queryAccount: (
+          context: Context,
+          address: string | Uint8Array
+        ): Promise<Account> => {
+          return this.rest.getAccount(address);
+        },
+        bech32Config: defaultBech32Config("cosmos"),
+        bip44: new BIP44(44, 118, 0)
       },
-      queryAccount: (
-        context: Context,
-        address: string | Uint8Array
-      ): Promise<Account> => {
-        return this.rest.getAccount(address);
-      },
-      bech32Config: defaultBech32Config("cosmos"),
-      bip44: new BIP44(44, 118, 0)
+      ...coreConfig
     });
   }
 }
