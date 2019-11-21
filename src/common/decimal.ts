969ab00bd4cafc602a6931632d9ee28542d02f81
Thunnini <yunjh1994@gmail.com>
2019-11-21 22:51:06 +0900

    Add some calculation methods to Dec

--- src/common/decimal.ts
+++ src/common/decimal.ts
@@ -8,7 +8,7 @@ export class Dec {
   public static readonly precision: bigInteger.BigInteger = bigInteger(18);
   private static readonly precisionMultipliers: {
     [key: string]: bigInteger.BigInteger | undefined;
-  };
+  } = {};
   private static calcPrecisionMultiplier(
     prec: bigInteger.BigInteger
   ): bigInteger.BigInteger {
@@ -38,6 +38,9 @@ export class Dec {
    */
   constructor(int: bigInteger.BigNumber | Int, prec: number = 0) {
     if (typeof int === "string") {
+      if (int.length === 0) {
+        throw new Error("empty string");
+      }
       if (int.indexOf(".") >= 0) {
         prec = int.length - int.indexOf(".") - 1;
         int = int.replace(".", "");
@@ -52,6 +55,222 @@ export class Dec {
     }
 
     this.int = this.int.multiply(Dec.calcPrecisionMultiplier(bigInteger(prec)));
+  }
+
+  public isZero(): boolean {
+    return this.int.eq(bigInteger(0));
+  }
+
+  public isNegative(): boolean {
+    return this.int.isNegative();
+  }
+
+  public isPositive(): boolean {
+    return this.int.isPositive();
+  }
+
+  public equals(d2: Dec): boolean {
+    return this.int.eq(d2.int);
+  }
+
+  /**
+   * Alias for the greater method.
+   */
+  public gt(d2: Dec): boolean {
+    return this.int.gt(d2.int);
+  }
+
+  /**
+   * Alias for the greaterOrEquals method.
+   */
+  public gte(d2: Dec): boolean {
+    return this.int.geq(d2.int);
+  }
+
+  /**
+   * Alias for the lesser method.
+   */
+  public lt(d2: Dec): boolean {
+    return this.int.lt(d2.int);
+  }
+
+  /**
+   * Alias for the lesserOrEquals method.
+   */
+  public lte(d2: Dec): boolean {
+    return this.int.leq(d2.int);
+  }
+
+  /**
+   * reverse the decimal sign.
+   */
+  public neg(): Dec {
+    return new Dec(this.int.negate(), Dec.precision.toJSNumber());
+  }
+
+  /**
+   * Returns the absolute value of a decimals.
+   */
+  public abs(): Dec {
+    return new Dec(this.int.abs(), Dec.precision.toJSNumber());
+  }
+
+  public add(d2: Dec): Dec {
+    return new Dec(this.int.add(d2.int), Dec.precision.toJSNumber());
+  }
+
+  public sub(d2: Dec): Dec {
+    return new Dec(this.int.subtract(d2.int), Dec.precision.toJSNumber());
+  }
+
+  public mul(d2: Dec): Dec {
+    return new Dec(
+      this.mulRaw(d2).chopPrecisionAndRound(),
+      Dec.precision.toJSNumber()
+    );
+  }
+
+  public mulTruncate(d2: Dec): Dec {
+    return new Dec(
+      this.mulRaw(d2).chopPrecisionAndTruncate(),
+      Dec.precision.toJSNumber()
+    );
+  }
+
+  private mulRaw(d2: Dec): Dec {
+    return new Dec(this.int.multiply(d2.int), Dec.precision.toJSNumber());
+  }
+
+  public quo(d2: Dec): Dec {
+    return new Dec(
+      this.quoRaw(d2).chopPrecisionAndRound(),
+      Dec.precision.toJSNumber()
+    );
+  }
+
+  public quoTruncate(d2: Dec): Dec {
+    return new Dec(
+      this.quoRaw(d2).chopPrecisionAndTruncate(),
+      Dec.precision.toJSNumber()
+    );
+  }
+
+  public quoRoundUp(d2: Dec): Dec {
+    return new Dec(
+      this.quoRaw(d2).chopPrecisionAndRoundUp(),
+      Dec.precision.toJSNumber()
+    );
+  }
+
+  private quoRaw(d2: Dec): Dec {
+    const precision = Dec.calcPrecisionMultiplier(bigInteger(0));
+
+    // multiply precision twice
+    const mul = this.int.multiply(precision).multiply(precision);
+    return new Dec(mul.divide(d2.int), Dec.precision.toJSNumber());
+  }
+
+  public isInteger(): boolean {
+    const precision = Dec.calcPrecisionMultiplier(bigInteger(0));
+    return this.int.remainder(precision).equals(bigInteger(0));
+  }
+
+  /**
+   * Remove a Precision amount of rightmost digits and perform bankers rounding
+   * on the remainder (gaussian rounding) on the digits which have been removed.
+   */
+  private chopPrecisionAndRound(): bigInteger.BigInteger {
+    // Remove the negative and add it back when returning
+    if (this.isNegative()) {
+      const absoulteDec = this.abs();
+      const choped = absoulteDec.chopPrecisionAndRound();
+      return choped.negate();
+    }
+
+    const precision = Dec.calcPrecisionMultiplier(bigInteger(0));
+    const fivePrecision = precision.divide(bigInteger(2));
+
+    // Get the truncated quotient and remainder
+    const { quotient, remainder } = this.int.divmod(precision);
+
+    // If remainder is zero
+    if (remainder.equals(bigInteger(0))) {
+      return quotient;
+    }
+
+    if (remainder.lt(fivePrecision)) {
+      return quotient;
+    } else if (remainder.gt(fivePrecision)) {
+      return quotient.add(bigInteger(1));
+    } else {
+      // always round to an even number
+      if (quotient.divide(bigInteger(2)).equals(bigInteger(0))) {
+        return quotient;
+      } else {
+        return quotient.add(bigInteger(1));
+      }
+    }
+  }
+
+  private chopPrecisionAndRoundUp(): bigInteger.BigInteger {
+    // Remove the negative and add it back when returning
+    if (this.isNegative()) {
+      const absoulteDec = this.abs();
+      // truncate since d is negative...
+      const choped = absoulteDec.chopPrecisionAndTruncate();
+      return choped.negate();
+    }
+
+    const precision = Dec.calcPrecisionMultiplier(bigInteger(0));
+
+    // Get the truncated quotient and remainder
+    const { quotient, remainder } = this.int.divmod(precision);
+
+    // If remainder is zero
+    if (remainder.equals(bigInteger(0))) {
+      return quotient;
+    }
+
+    return quotient.add(bigInteger(1));
+  }
+
+  /**
+   * Similar to chopPrecisionAndRound, but always rounds down
+   */
+  private chopPrecisionAndTruncate(): bigInteger.BigInteger {
+    const precision = Dec.calcPrecisionMultiplier(bigInteger(0));
+    return this.int.divide(precision);
+  }
+
+  public toString(): string {
+    const precision = Dec.calcPrecisionMultiplier(bigInteger(0));
+    const int = this.int.abs();
+    const { quotient: interger, remainder: fraction } = int.divmod(precision);
+
+    let fractionStr = fraction.toString(10);
+    for (
+      let i = 0, l = fractionStr.length;
+      i < Dec.precision.toJSNumber() - l;
+      i++
+    ) {
+      fractionStr = "0" + fractionStr;
+    }
+
+    return (
+      (this.isNegative() ? "-" : "") + interger.toString(10) + "." + fractionStr
+    );
+  }
+
+  public round(): Int {
+    return new Int(this.chopPrecisionAndRound());
+  }
+
+  public roundUp(): Int {
+    return new Int(this.chopPrecisionAndRoundUp());
+  }
+
+  public truncate(): Int {
+    return new Int(this.chopPrecisionAndTruncate());
   }
 
   @Method.AminoMarshaler({ type: Type.String })
